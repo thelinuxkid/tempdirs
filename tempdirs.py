55282f616ea64685f24c3d9f48c3cf24c6756910
Andres Buritica <andres@thelinuxkid.com>
2012-10-18 00:00:16 -0700

    makedirs works both as a decorator and a contextmanager

--- tempdirs.py
+++ tempdirs.py
@@ -1,31 +1,62 @@
+import sys
 import functools
 import tempfile
 import shutil
+import errno
 
-class makedirs(object):
-    def __init__(self, num=1):
-        self._num = num
+NO_EXC = (None, None, None)
+
+# Inspiration from http://code.google.com/p/contextdecorator
+class DecoratorContext(object):
+    def decorated_fn(self, fn, *args, **kwargs):
+        return fn(*args, **kwargs)
 
     def __call__(self, fn):
         @functools.wraps(fn)
         def wrapper(*args, **kwargs):
-            def manager():
-                try:
-                    dirs = [
-                        tempfile.mkdtemp()
-                        for i in xrange(self._num)
-                        ]
-                    extra_args = list(args)
-                    dirs += extra_args
-                    fn(*dirs, **kwargs)
-                finally:
-                    for dir_ in dirs:
-                        try:
-                            shutil.rmtree(dir_)
-                        except OSError, e:
-                            # It's OK if dir doesn't exist
-                            if e.errno != 2:
-                                raise
-
-            return manager()
+            exc = NO_EXC
+            result = None
+            try:
+                self.__enter__()
+                result = self.decorated_fn(fn, *args, **kwargs)
+            except Exception:
+                exc = sys.exc_info()
+            finally:
+                supress = self.__exit__(*exc)
+                if not supress and exc is not NO_EXC:
+                    (cls, val, tb) = exc
+                    raise cls, val, tb
+            return result
         return wrapper
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        return False
+
+class makedirs(DecoratorContext):
+    def __init__(self, num=1):
+        self._num = num
+
+    def decorated_fn(self, fn, *args, **kwargs):
+        extra_args = list(args)
+        extra_args = self._dirs + extra_args
+        return fn(*extra_args, **kwargs)
+
+    def __enter__(self):
+        self._dirs = [
+            tempfile.mkdtemp()
+            for i in xrange(self._num)
+        ]
+        return self._dirs
+
+    def __exit__(self, *exc):
+        for dir_ in self._dirs:
+            try:
+                shutil.rmtree(dir_)
+            except OSError, e:
+                # It's OK if dir doesn't exist
+                if e.errno != errno.ENOENT:
+                    raise
+        return False
