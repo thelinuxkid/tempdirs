1a689a75ddef9c6e0dbb89338dc2041c34c90a17
Andres Buritica <andres@thelinuxkid.com>
2012-10-18 00:00:57 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tempdirs',
-    version='0.0.2',
+    version='0.0.3',
     description='tempdirs -- Safely create temporary directories',
     long_description=(
         'tempdirs is a library which allows users to safely and '
