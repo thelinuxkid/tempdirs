26b9edf34e126bf48c3e66f25cf34d991360b8e2
Andres Buritica <andres@thelinuxkid.com>
2012-10-20 01:03:15 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 from setuptools import setup, find_packages
+import os
 
 # Pypi package documentation
-with open('README.rst') as fp:
+root = os.path.dirname(__file__)
+path = os.path.join(root, 'README.rst')
+with open(path) as fp:
     long_description = fp.read()
 
 setup(
     name='tempdirs',
-    version='0.0.5',
+    version='0.0.6',
     description='tempdirs -- Safely create temporary directories',
     long_description=long_description,
     author='Andres Buritica',
@@ -15,6 +18,7 @@ setup(
     maintainer='Andres Buritica',
     maintainer_email='andres@thelinuxkid.com',
     url='https://github.com/thelinuxkid/tempdirs',
+    license='MIT',
     packages = find_packages(),
     py_modules=['tempdirs'],
     classifiers=[
